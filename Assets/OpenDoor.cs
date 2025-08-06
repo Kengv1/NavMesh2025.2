bda6bb9aa5d5b48b737afd6a9582434551940fbe
YuKang Peng <p237793151@gmail.com>
2025-08-06 15:58:34 +1000

    added another navmesh agent

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Vector3 positionDelta = new Vector3(0, -2f, 0);
+    public float speed = 5f;
+    public float waitTime = 3f;
+
+    private Vector3 closedPosition;
+    private Vector3 openPosition;
+
+    bool isOpen = false;
+    float startTime = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        closedPosition = transform.position;
+        openPosition = transform.position + positionDelta;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isOpen)
+        {
+
+
+            if (Time.time > startTime + waitTime)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, openPosition, speed * Time.deltaTime);
+            }
+            if (Vector3.Distance(transform.position, openPosition) < 0.1f)
+            {
+
+                isOpen = true;
+                startTime = Time.time;
+            }
+        }
+        else {
+
+            if (Time.time > startTime + waitTime)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, closedPosition, speed * Time.deltaTime);
+            }
+            if (Vector3.Distance(transform.position, closedPosition) < 0.1f)
+            {
+
+                isOpen = false;
+                startTime = Time.time;
+            }
+        }
+    }
+        
+    }
+}
